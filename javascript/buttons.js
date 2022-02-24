aaf5804fb84168a25d15433c1d8e69c7e96386ea
Saawan Fultariya <saawan.fultariya@kcsitglobal.com>
2022-02-24 17:51:29 +0530

    Added js and jQuery

---
+++ javascript/buttons.js
@@ -0,0 +1,12 @@
+function buttonalertexternal() {
+    alert('Here is alert');
+
+}
+function buttonconfirmexternal() {
+    confirm('here is confirm');
+
+}
+function buttonpromptexternal() {
+
+    prompt('here is prompt');
+}
